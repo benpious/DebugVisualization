e193a2e0108aeec82ad2276cc7d7aa4c71b87e4a
benpious <benpious@gmail.com>
2020-03-07 17:38:25 -0800

    add remangling

--- send_command.py
+++ send_command.py
@@ -2,25 +2,30 @@ import lldb
 import asyncio
 
 def send_visual(debugger, var_name, result, internal_dict):
-    cmd = 'expr try JSONEncoder().encode(' + var_name + ')'
     debugger = lldb.debugger.GetCommandInterpreter()
+    output = expr(debugger, """import Darwin
+let data = try JSONEncoder().encode({name})
+let string = String(data: data, encoding: .utf8)!
+let type: Any.Type = Swift.type(of: {name})
+let pointer = UnsafeRawPointer(bitPattern: unsafeBitCast(type, to: Int.self))
+var info = Dl_info()
+dladdr(pointer, &info)
+let fileName = String(cString: info.dli_fname)
+let mangledName = String(cString: info.dli_sname)
+"\(fileName), \(mangledName), \(string)"
+        """.format(name=var_name))
+    asyncio.run(send(output))
+
+
+def expr(debugger, cmd):
+    cmd = 'po ' + ';'.join(cmd.split('\n')) # unclear if this is necessary, but was the only way I could get it to work
     result = lldb.SBCommandReturnObject()
     debugger.HandleCommand(cmd, result)
     if result.Succeeded():
-        output = result.GetOutput()
-        print(output)
-        if output.startswith('(Data) '):
-            var_name = output[7:11]
-            cmd = 'po String(data: ' + var_name + ', encoding: .utf8)!'
-            debugger.HandleCommand(cmd, result)
-            if result.Succeeded():
-                output = result.GetOutput()
-                asyncio.run(send(output))
-            else:
-                print(result.GetError())
+        return result.GetOutput()
     else:
         print(result.GetError())
-
+        raise
 
 async def send(message):
     _, writer = await asyncio.open_connection('localhost', 7000)
