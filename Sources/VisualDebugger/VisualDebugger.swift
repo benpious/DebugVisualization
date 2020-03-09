67e8e3092967f2a694c7e0733036bad90b94cb5c
benpious <benpious@gmail.com>
2020-03-08 19:29:17 -0700

    add documentation

--- Sources/VisualDebugger/VisualDebugger.swift
+++ Sources/VisualDebugger/VisualDebugger.swift
@@ -1,8 +1,14 @@
 import SwiftUI
 
+/// Entrypoint to the package.
+///
+/// You initialize this class with `LLDBStream()`,
+/// and must then call `start()` to start listening for
+/// debugger messages, and `makeView()` to get a view
+/// that you can add to your window hierarchy in an `NSHostingView`.
 public class VisualDebugger {
     
-    let lldbStream: LLDBStream
+    private let lldbStream: LLDBStream
     
     public init() {
         lldbStream = LLDBStream(port: 7000)
