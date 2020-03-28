b1ddca8281a2828b4c78de42fdff2dbc91cec245
benpious <benpious@gmail.com>
2020-03-28 10:57:08 -0700

    remove comment

--- Package.swift
+++ Package.swift
@@ -16,8 +16,6 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "VisualDebugger",
             dependencies: ["NIO"]),
